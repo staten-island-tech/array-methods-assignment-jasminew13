c1e729eeffdab0dd6194218e07616d7e3af78593
jasminew13 <jasminewu101@gmail.com>
2023-10-12 09:43:25 -0400

    OEIFHOIWEJFE

--- objects.js
+++ objects.js
@@ -1,40 +1,41 @@
 const artists = [
     {
     Artist: "taylor swift",
+    Genre: ["pop", "country", "pop rock"],
     Age: 33,
     LatestAlbum: "speak now (taylors version)",
     },
     {
     Artist: "keshi",
-    Genre: "r&b",
+    Genre: ["r&b"],
     Age: 28,
     LatestAlbum: "gabriel",
     },
     {
     Artist: "mitski",
-    Genre: "alt indie",
+    Genre: ["indie"],
     Age: 33,
     LatestAlbum: "the land is inhospitable and so are we",
     },
     {
     Artist: "matt maltese",
-    Genre: "alt indie",
+    Genre: ["alt indie"],
     Age: 26,
     LatestAlbum: "krystal",
     },
     {
     Artist: "babygirl",
-    Genre: "indie pop",
+    Genre: ["indie pop"],
     Age: 24,
     LatestAlbum: "be still my heart",
     },
-]
-artists.forEach((el)=> console.log(el.Artist))
-const taylor = ["pop", "country", "pop rock"];
-console.log(taylor[0]);
-console.log(taylor[1]);
-console.log(taylor[2]);
-console.log(taylor[3]);
+];
 
+
+artists.forEach((artist) => 
+ artist.Genre.forEach((genre) => console.log(genre))
+)
+
+artists.forEach((artist)=> console.log(artist.LatestAlbum))
 const results = artists.filter((artist) => artist.Age == 33);
 console.log(results);
